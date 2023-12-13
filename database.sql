e987b31edc5991ea9c199a8b6b9d9930f30fe807
three-days-and-three-nights <1692565761@qq.com>
2023-12-13 17:44:10 +0800

    update database.sql

---
+++ database.sql
@@ -0,0 +1,11 @@
+create table botinformation
+(
+    BotID        int auto_increment
+        primary key,
+    BotName      varchar(20)                            null,
+    BotKey       varchar(255)                           null,
+    BotStatus    tinyint(1) default 1                   null,
+    MessageCount int        default 0                   null,
+    CreatedAt    timestamp  default current_timestamp() null
+);
+
